a0d7ff759b9db07aae546c96078c6e23732ec5a8
kandori-31 <ezynmthd@i.softbank.jp>
2020-05-07 00:49:27 +0900

    投稿画面のルーティング

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,18 @@ Route::get('/', function () {
     $Tweets = \App\Tweet::all();
     return view('index', ['Tweets' => $Tweets]);
 });
+
+use Illuminate\Http\Request;
+
+Route::get('/submit', function(Request $request){
+    $data = $request->validate([
+        'title' => 'required | max:255',
+        'image'  => 'required | url | max:255',
+        'text' => 'required | max:255',
+    ]);
+
+    $Tweet = new App\Tweet($data);
+    $Tweet->save();
+
+    return redirect('/');
+});
