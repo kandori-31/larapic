739f8010989cdb87d2ad36b1956596e6ac23979f
kandori-31 <ezynmthd@i.softbank.jp>
2020-05-09 19:12:43 +0900

    ツイートコントローラー作成

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -2,18 +2,101 @@
 
 namespace App\Http\Controllers;
 
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
+
 use App\Tweet;
-use App\Http\Requests\TweetRequest;
+
+use App\Http\Requests\LinkRequest;
 
 class TweetController extends Controller
 {
-    public function submit(TweetRequest $request){
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {   
+        $tweets = Tweet::paginate(5);
+        // $tweets = \App\Tweet::all();
+        // $users = $tweets->orderBy('id','desc')->paginate(10);
+        return view('index', [
+            'Tweets' => $tweets
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
         $Tweet = new Tweet();
         $Tweet->title = $request->title;
         $Tweet->image = $request->image;
         $Tweet->text = $request->text;
         $Tweet->save();
-        return redirect('/');
+        return redirect('/tweets');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $tweet = Tweet::find($id);
+        return view('show',[
+            "tweet" => $tweet
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $tweet = Tweet::find($id)
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $tweet = Tweet::find($id);
+        $tweet->delete();
     }
 }
