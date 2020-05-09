15a9965b297b2cf3b5c642d2176b58f6dbf3f447
kandori-31 <ezynmthd@i.softbank.jp>
2020-05-09 19:11:19 +0900

    ツイート一覧　ページネーション

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,14 @@
 
 @section('content')
 <div class="contents row">
-        @foreach ($Tweets as $Tweet)
-            <div class="content_post" style="background-image: url({{ $Tweet -> image }});">
-            <p>{{ $Tweet -> text }}</p>
-            <span class="name">
-            {{ $Tweet -> title }}
-            </span>
-            </div>
-        @endforeach
+    @foreach ($Tweets as $Tweet)
+        <div class="content_post" style="background-image: url({{ $Tweet -> image }});">
+        <p>{{ $Tweet -> text }}</p>
+        <span class="name">
+        {{ $Tweet -> title }}
+        </span>
+        </div>
+    @endforeach
+    {{ $Tweets->links() }}
 </div>
 @endsection
