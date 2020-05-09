f1cbe56f1edc2b030c49aec01f4673f1690f6e72
kandori-31 <ezynmthd@i.softbank.jp>
2020-05-09 19:11:47 +0900

    投稿画面　ファイル名変更

---
+++ resources/views/create.blade.php
@@ -0,0 +1,29 @@
+@extends ('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <h3>投稿する</h3>
+        <form action="/tweets" method="post">
+            {!! csrf_field() !!}
+
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" class="form-control" id="title" name="title" placeholder="Title">
+            </div>
+
+            <div class="form-group">
+            <label for="image">Url</label>
+                <input type="text" class="form-control" id="imgae" name="image" placeholder="Image URL">
+            </div>
+
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea class="form-control" id="description" name="text" placeholder="text"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+    </div>
+  </div>
+@endsection
