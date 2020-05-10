9d30ee3bc666d600bbcee3e2d946f947766eedb7
kandori-31 <ezynmthd@i.softbank.jp>
2020-05-10 23:29:06 +0900

    tweet_show

---
+++ resources/views/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+
+@section('content')
+<div class ="contents row">
+<div class="content_post" style="background-image: url({{ $tweet -> image }});">
+        <div class="more">
+        <span><img src = "/images/arrow_top.png"></span>
+        <ul class="more_list">
+            <li>
+            <a href="/tweets/{{ $tweet -> id }}/edit">編集</a>
+            <li>
+            <form action="/tweets/{{ $tweet -> id }}" method = "post">
+            {{ csrf_field() }}
+            {{ method_field('delete') }}
+            <button>削除する</button>
+            </form>
+            </li>
+        </ul>
+        </div>
+        <p>{{ $tweet -> text }}</p>
+        <span class="name">
+        {{ $tweet -> title }}
+        </span>
+</div>
+@endsection
