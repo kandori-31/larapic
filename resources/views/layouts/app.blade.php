818255f29a49211b3972ddf60261728df7482236
kandori-31 <ezynmthd@i.softbank.jp>
2020-05-11 22:02:53 +0900

    ログインに関するリンクを作成
    現在の状態で表示を変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,17 +13,36 @@
 
 </head>
 <body>
-<header class="header">
-      <div class="header__bar row">
-        <h1 class="grid-6"><a href="/tweets">Larapic</a></h1>
-        <div class="user_nav grid-6">
-          <a class="post" href="/tweets/create">投稿する</a>
-          <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-          <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-        </div>
+  <header class="header">
+    <div class="header__bar row">
+      <h1 class="grid-6"><a href="/tweets">Larapic</a></h1>
+      <div class="user_nav grid-6">
+        @guest
+          <a class="nav-links" href="{{ route('login') }}">{{ __('Login') }}</a>
+          @if (Route::has('register'))
+            <a class="nav-links" href="{{ route('register') }}">{{ __('Register') }}</a>
+          @endif
+        @else
+        
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+          <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#">
+            {{ Auth::user()->name }} 
+          </a>
+          <a class="dropdown-item" href="{{ route('logout') }}"
+          onclick="event.preventDefault();
+                                  document.getElementById('logout-form').submit();">
+                  {{ __('Logout') }}
+                </a>
+                
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+                </form>
+                <a class="post" href="/tweets/create">投稿する</a>
+              </div>
+        @endguest              
       </div>
-    </header>
-   
+    </div>
+  </header>
     {{-- コンテンツの表示 --}}
     <!-- <div class="container"> -->
         @yield('content')
--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     $Tweets = \App\Tweet::all();
-//     return view('index', ['Tweets' => $Tweets]);
-// });
+
 Route::resource('tweets', 'TweetController');
-// Route::get('/tweets','TweetController@index');
-// Route::get('/tweets/create', 'TweetController@create');
-// Route::post('/tweets','TweetController@store');
-// Route::get('/tweets/{id}', 'TweetController@show');
-// Route::delete('/tweets/destroy/{id}', 'TweetController@destroy');
-// Route::get('/submit', function () {
-//     return view('submit');
-// });
-// use Illuminate\Http\Request;
-// Route::post('/submit','TweetController@submit');
 
 Auth::routes();
 
