2da9482e5d2c1e6df1c352030531a9b53d33ac49
kandori-31 <ezynmthd@i.softbank.jp>
2020-05-16 21:03:20 +0900

    外部キーに対応

--- database/migrations/2020_05_06_060304_create_tweets_table.php
+++ database/migrations/2020_05_06_060304_create_tweets_table.php
@@ -14,7 +14,7 @@ class CreateTweetsTable extends Migration
     public function up()
     {
         Schema::create('tweets', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users'); 
             $table->string('title');
