aee737bda80036dad376cb4d4adaa4603c7742b0
kandori-31 <ezynmthd@i.softbank.jp>
2020-05-16 21:03:40 +0900

    commentsテーブル作成

---
+++ database/migrations/2020_05_16_092329_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users'); 
+            $table->integer('tweet_id')->unsigned();
+            $table->foreign('tweet_id')->references('id')->on('tweets'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
