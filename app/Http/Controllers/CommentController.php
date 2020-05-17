3413caf40f767070ee0164d7aba695b8cac121dc
kandori-31 <ezynmthd@i.softbank.jp>
2020-05-17 22:00:38 +0900

    コメント保存　アクション

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,7 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Tweet;
+
+use App\Comment;
+
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request,$id){
+        $comment = new Comment();
+        $comment->user_id = $request->user()->id;
+        $comment->tweet_id = $id;
+        $comment->text = $request->text;
+        $comment->save();
+        return back()->withInput();
+    }
 }
