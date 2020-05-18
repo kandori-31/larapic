7fc67030e7b61214d9073d10b26de0cac3e4e54e
kandori-31 <ezynmthd@i.softbank.jp>
2020-05-19 01:29:51 +0900

    検索のメソッド

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,7 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+
 class SearchController extends Controller
 {
-    //
+    public function index(Request $request){
+        $keyword = $request->input('keyword');
+        $tweets = DB::table('tweets')
+        ->where('text', 'like', '%'.$keyword.'%')
+        ->paginate(5);
+        return view('search.index',[
+            'tweets' => $tweets,
+            'keyword' => $keyword,
+            ]);
+    
+    }
 }
