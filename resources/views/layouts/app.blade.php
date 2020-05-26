f63d092b940ea7661d46a8e707e9ff5bfd0b097a
kandori-31 <ezynmthd@i.softbank.jp>
2020-05-26 22:38:48 +0900

    js読み込み　csrf設定

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,9 @@
     <title>Larapic</title>
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/comment.js') }}" defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
   <header class="header">
@@ -56,4 +56,5 @@
       </p>
     </footer>
 </div>
+@yield('js')
 </body>
